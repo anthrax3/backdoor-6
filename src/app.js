08dd21522b273b08e212c3904707425eabe9df2b
yuchao <553216263@qq.com>
2018-03-28 15:50:28 +0800

    add new files

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const chalk = require('chalk');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end('Hello NodeJS!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server started at: http://${chalk.green(hostname)}:${chalk.green(port)}`);
+});
